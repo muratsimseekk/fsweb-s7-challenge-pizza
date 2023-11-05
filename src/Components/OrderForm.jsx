e37c0a6567a6eae467e4e6ae42796f017ceeb70a
Murat Simsek <muratsimseekk@gmail.com>
2023-11-05 15:01:25 +0300

    OrderForm structure is done

--- src/Components/OrderForm.jsx
+++ src/Components/OrderForm.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const OrderForm = () => {
   return (
-    <div className="w-screen h-[100vh] bg-red-600">
-      <div></div>
-      <div></div>
+    <div className="w-screen h-[100vh] bg-red-600 flex flex-col items-center">
+      <div className="w-[100%] h-[16vh] bg-slate-300"></div>
+      <div className="w-[40%] h-[16vh] bg-gray-600 absolute"></div>
+      <div className="w-[40%] h-[82vh] bg-blue-400"></div>
     </div>
   );
 };
