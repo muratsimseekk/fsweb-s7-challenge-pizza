2c657c9eb24dd5adb83696734b6396bf75189f11
Murat Simsek <muratsimseekk@gmail.com>
2023-11-08 20:54:12 +0300

    summary page creates and styles

--- src/Components/OrderReceived.jsx
+++ src/Components/OrderReceived.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const OrderReceived = () => {
-  return <div>OrderReceived</div>;
+  return (
+    <>
+      <div className="bg-red-600 w-screen h-[100vh] flex justify-center items-center">
+        <div className="w-1/3 h-3/5  mb-28 flex flex-col justify-between">
+          <div className="font-londrina text-[34px] text-center text-white">
+            Teknolojik Yemekler
+          </div>
+          <div className="font-bebas text-[55px] text-white text-center tracking-wider leading-[92px]">
+            tebrikler! <br />
+            Siparisiniz alindi!
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrderReceived;
